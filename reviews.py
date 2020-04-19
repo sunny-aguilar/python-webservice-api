ab8f94051394864d6daf069e59d5e47b0689ef20
sunny-aguilar <sunny.aguilar@gmail.com>
2020-04-19 15:09:02 -0700

    web api

--- reviews.py
+++ reviews.py
@@ -23,34 +23,19 @@ feedback_list = []
 feedback = {}
 
 for file in files:
-    with open('data/feedback/'+file, 'r') as current_file:
+    with open('data/feedback/'+file, 'r+') as current_file:
     # f = open('data/feedback/' + file)
         for line in current_file:
             field = line.strip()
-            print(field)
+            # print(line)
             feedback[field_names[index]] = field
             index += 1
-            feedback_list.append(feedback)
         print(feedback_list)
         index = 0
-
-# for file in files:
-#     with open('data/feedback/'+file, 'r') as current_file:
-#     # f = open('data/feedback/' + file)
-#         for line in current_file:
-#             field = line.strip()
-#             # print(field_names[index])
-#             feedback[field_names[index]] = field
-#             index += 1
-#         feedback_list.append(feedback)
-#         index = 0
 
 # print(feedback_list)
 
 
 # serialize data
-# feedback_json = json.dumps(feedback_list)
-# print(feedback_json)
-
-
-
+# with open('feedback_list.json', 'w') as serial_feedback:
+#     json.dump(feedback_list, serial_feedback)
