d63aeb9dbfb4b7ca2e8581cb58bc11ac979bf09f
sunny-aguilar <sunny.aguilar@gmail.com>
2020-04-19 12:22:37 -0700

    add .py file

---
+++ reviews.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#
+#   A script that iterates over image files in a directory and makes changes
+#   to them using the Pillow library. The images are first rotated 90 degrees,
+#   resized to 128 x 128 and then saved as .jpeg image files.
+#
+#   Author:     Sandro Aguilar
+#   Date:       April 15, 2020
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
